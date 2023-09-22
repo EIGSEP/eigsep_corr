b1a6d0ed9c880c046ff60f02bc090adad6249991
Christian Hellum Bye <chbye@berkeley.edu>
2023-09-22 16:34:43 -0700

    add init_fem method

--- eigsep_corr/fpga.py
+++ eigsep_corr/fpga.py
@@ -21,6 +21,7 @@ class EigsepFpga:
             self.fpg_file = fpg_file
             self.fpga.upload_to_ram_and_program(self.fpg_file)
 
+        # blocks
         self.synth = casperfpga.synth.LMX2581(self.fpga, "synth")
         self.adc = casperfpga.snapadc.SnapAdc(
             self.fpga, num_chans=2, resolution=8, ref=10
@@ -29,6 +30,13 @@ class EigsepFpga:
         self.inp = Input(self.fpga, "input", nstreams=12)
         self.noise = NoiseGen(self.fpga, "noise", nstreams=6)
         self.pfb = Pfb(self.fpga, "pfb")
+        # self.delay = Delay(self.fpga, "delay", nstreams=6)
+        # self.eq = Eq(self.fpga, "eq_core", nstreams=6, ncoeffs=2**10)
+        # self.reorder = ChanReorder(self.fpga, "chan_reorder", nchans=2**10)
+        # self.packetizer = Packetizer(self.fpga, "packetizer", n_time_demux=2)
+        # self.eth = Eth(self.fpga, "eth")
+        # self.corr = Corr(self.fpga, "corr_0")
+        # self.phase_switch = PhaseSwitch(self.fpga, "phase_switch")
 
         self.autos = [0, 1, 2, 3, 4, 5]
         self.crosses = ["02", "13", "24", "35", "04", "15"]
@@ -86,6 +94,20 @@ class EigsepFpga:
         for pam in self.pams:
             pam.initialize()
             pam.set_attenuation(8, 8)  # XXX
+    
+    def initialize_fems(self, N=3):
+        """
+        Initialize the FEMs.
+
+        Parameters
+        ----------
+        N : int
+           Number of FEMs to initialize. Default is 3.
+
+        """
+        self.fems = [Fem(self.fpga, f"i2c_ant{i}") for i in range(N)]
+        for fem in self.fems:
+            fem.initialize()
 
     def initialize_blocks(
         self,
@@ -95,21 +117,29 @@ class EigsepFpga:
         corr_acc_len=2**28,
         corr_scalar=2**9,
         n_pams=3,
+        n_fems=3,
     ):
-        # initialize adc
         # self.synth.initialize()  # XXX is this necessary?
         self.initialize_adc(adc_sample_rate, adc_gain)
-        # set register values
         self.sync.initialize()
-        self.inp.initialize()
         self.noise.initialize()
+        self.inp.initialize()
+        # self.delay.initialize()
         self.pfb.initialize()
         self.pfb.set_fft_shift(pfb_fft_shift)
-        # initialize correlator
+        # self.eq.initialize()
+        # self.reorder.initialize()
+        # self.packetizer.initialize()
+        # self.eth.initialize()
+        # self.corr.initialize()
+        # self.phase_switch.initialize()
         self.initialize_fpga(corr_acc_len, corr_scalar)
         # initialize pams
         if n_pams > 0:
             self.initialize_pams(N=n_pams)
+        # initialize fems
+        if n_fems > 0:
+            self.initialize_fems(N=n_fems)
 
     def synchronize(self, delay=0):
         self.sync.set_delay(delay)
