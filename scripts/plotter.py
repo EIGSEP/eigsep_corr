3c11df963f0e98b66d5c430ab05f9d985a271466
Christian Hellum Bye <chbye@berkeley.edu>
2023-09-29 17:44:08 -0700

    connect remotely

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -1,9 +1,9 @@
 import redis
 from eigsep_corr.plot import plot
 
-REDIS_HOST = "localhost"
+REDIS_HOST = "192.168.0.116"
 REDIS_PORT = 6379
 
 r = redis.Redis(REDIS_HOST, port=REDIS_PORT)
 
-plot(r, ylim_mag=(0, 1e5))
+plot(r, pairs=["0", "2", "02"], ylim_phase=(-5, 5))
