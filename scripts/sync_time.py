07e7e2b29a6aa0fec6979b33a66023d2210fad52
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-07-01 16:26:41 -0700

    bug fix with print statement

--- scripts/sync_time.py
+++ scripts/sync_time.py
@@ -1,7 +1,6 @@
 import paramiko
 import time
 import argparse
-from datetime import datetime
 
 # SSH credentials
 username = 'eigsep'
@@ -17,14 +16,22 @@ def sync_time(host):
         print(f'Connected to {host}')
         ssh.exec_command(f'sudo date -s "{current_time}"')
         print(f'Time synced with {host}')
+        
         # Run a command to check the time on the Pi
         stdin, stdout, stderr = ssh.exec_command(
-            'python3 -c "import time; from datetime import datetime; now = time.time(); '
+            'python3 -c "import time; from datetime import datetime; '
+            'now = time.time(); '
             'print(f\'Epoch time: {now}\'); '
             'print(f\'Human-readable time: {datetime.fromtimestamp(now).strftime("%Y-%m-%d %H:%M:%S")}\')"'
         )
-        synced_time_output = stdout.read().decode().strip()
-        print(f"Current time on {host}:\n{synced_time_output}")
+        stdout_str = stdout.read().decode().strip()
+        stderr_str = stderr.read().decode().strip()
+        
+        if stdout_str:
+            print(f"Current time on {host}:\n{stdout_str}")
+        if stderr_str:
+            print(f"Error output from {host}:\n{stderr_str}")
+            
         ssh.close()
     except Exception as e:
         print(f'Failed to sync time with {host}: {e}')
