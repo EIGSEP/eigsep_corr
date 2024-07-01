596e964b7978116a1e2a76e5dfd9b6d84de6c4f5
Pranav Karthik <pranavkarthik@berkeley.edu>
2024-07-01 16:22:45 -0700

    human readable time

--- scripts/sync_time.py
+++ scripts/sync_time.py
@@ -1,22 +1,26 @@
 import paramiko
 import time
 import argparse
+from datetime import datetime
 
 # SSH credentials
 username = 'eigsep'
-password = 'universe'  # Replace with your actual password
+password = 'universe'
 
 def sync_time(host):
     current_time = time.strftime('%Y-%m-%d %H:%M:%S')
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
+        print(f'Connecting to {host}...')
         ssh.connect(host, username=username, password=password)
+        print(f'Connected to {host}')
         ssh.exec_command(f'sudo date -s "{current_time}"')
         print(f'Time synced with {host}')
-        # Optional: Run a command to check the time on the Pi
-        stdin, stdout, stderr = ssh.exec_command('python3 -c "import time; print(time.time())"')
-        print(f"Current time on {host}: {stdout.read().decode().strip()}")
+        # Run a command to check the time on the Pi
+        stdin, stdout, stderr = ssh.exec_command('python3 -c "import time; from datetime import datetime; now = time.time(); print(now, datetime.fromtimestamp(now).strftime(\'%Y-%m-%d %H:%M:%S\'))"')
+        synced_time_output = stdout.read().decode().strip()
+        print(f"Current time on {host}: {synced_time_output}")
         ssh.close()
     except Exception as e:
         print(f'Failed to sync time with {host}: {e}')
