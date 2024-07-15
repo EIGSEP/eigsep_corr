36bd7792a739f3c6f34755f23e0d9e6f766e082b
EIGSEP Pi <chb@berkeley.edu>
2024-07-14 18:09:51 -0700

    plot all autos

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -3,8 +3,8 @@ from eigsep_corr.plot import plot_live
 
 REDIS_HOST = "10.10.10.10"
 REDIS_PORT = 6379
+AUTOS = ["0", "1", "2", "3", "4", "5"]
+#pairs = ["2", "3"]
 
 r = redis.Redis(REDIS_HOST, port=REDIS_PORT)
-pairs = ["2", "3"]
-#pairs = ["0", "1", "2", "3", "4", "5"]
-plot_live(r, pairs=pairs, plot_delay=False, log_scale=False)
+plot_live(r, pairs=AUTOS, plot_delay=False, log_scale=False)
