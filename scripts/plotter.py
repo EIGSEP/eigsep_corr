b4aaec6d3950efbbaaeb4277c8a4532a2c8386e5
EIGSEP Pi <chb@berkeley.edu>
2024-07-15 16:27:51 -0700

    update what to plot

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -4,7 +4,7 @@ from eigsep_corr.plot import plot_live
 REDIS_HOST = "10.10.10.10"
 REDIS_PORT = 6379
 AUTOS = ["0", "1", "2", "3", "4", "5"]
-#pairs = ["2", "3"]
+pairs = ["2", "3"]
 
 r = redis.Redis(REDIS_HOST, port=REDIS_PORT)
-plot_live(r, pairs=AUTOS, plot_delay=False, log_scale=False)
+plot_live(r, pairs=pairs, plot_delay=False, log_scale=False)
