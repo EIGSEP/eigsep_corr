dc106f6d5868b8e9ecb190df5bf3ee1379a337ea
Christian Hellum Bye <chbye@berkeley.edu>
2024-07-16 10:04:48 -0700

    add read imu function

--- eigsep_corr/redis.py
+++ eigsep_corr/redis.py
@@ -4,10 +4,17 @@ autos = ["0", "1", "2", "3", "4", "5"]
 crosses = ["02", "04", "24", "13", "15", "35"]
 all_pairs = autos + crosses
 
+HOST = "10.10.10.10"
+PORT = 6379
 
-def grab_data(pairs=all_pairs, host="10.10.10.10", port=6379):
+def grab_data(pairs=all_pairs, host=HOST, port=PORT):
     r = redis.Redis(host=host, port=port)
     data = {}
     for p in pairs:
         data[p] = r.get(f"data:{p}")
     return data
+
+def grab_imu(host=HOST, port=PORT):
+    r = redis.Redis(host=host, port=port)
+    pos = {"theta": r.get("theta"), "phi": r.get("phi")}
+    return pos
