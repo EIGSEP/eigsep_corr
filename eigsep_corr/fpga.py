5518cab74df12908a5c6ae899eefa00c5922f8fc
Christian Hellum Bye <chbye@berkeley.edu>
2024-07-20 11:33:41 -0700

    add option to read accelerometer data and put in hdr

--- eigsep_corr/fpga.py
+++ eigsep_corr/fpga.py
@@ -60,6 +60,7 @@ class EigsepFpga:
         ref=None,
         transport=TapcpTransport,
         logger=None,
+        read_accelerometer=False,
     ):
         """
         Class for interfacing with the SNAP board.
@@ -80,6 +81,9 @@ class EigsepFpga:
             The transport protocol to use. The default is TapcpTransport.
         logger : logging.Logger
             The logger to use. If None, creates a new logger.
+        read_accelerometer : bool
+            Whether to read accelerometer data from the platform
+            FEM. Default is False.
 
         """
         if logger is None:
@@ -115,6 +119,12 @@ class EigsepFpga:
         self.queue = None
         self.event = None
 
+        # accelerometer data from FEM on platform
+        if read_accelerometer:
+            self.platform_redis = redis.Redis(host="10.10.10.12", port=6379)
+        else:
+            self.platform_redis = None
+
     @property
     def version(self):
         val = self.fpga.read_uint("version_version")
@@ -147,6 +157,12 @@ class EigsepFpga:
             }
         if self.is_synchronized:
             m["sync_time"] = self.sync_time
+        if self.platform_redis is not None:
+            accel = {
+                "theta": self.platform_redis.get("theta"),
+                "phi": self.platform_redis.get("phi"),
+            }
+            m["accelerometer"] = accel
         return m
 
     def _run_adc_test(self, test, n_tries):
@@ -444,6 +460,7 @@ class EigsepFpga:
         write_files=True,
         ntimes=io.DEFAULT_NTIMES,
         header=io.DEFAULT_HEADER,
+        read_accelerometer=False,
     ):
         """
         Observe continuously.
@@ -465,6 +482,9 @@ class EigsepFpga:
             io.DEFAULT_NTIMES (60).
         header : dict
             Header to write to each file. Default is io.DEFAULT_HEADER.
+        read_accelerometer : bool
+            Grab accelerometer data from redis and write to file. Default is
+            False.
 
         """
         self.queue = Queue(maxsize=0)  # XXX infinite size
