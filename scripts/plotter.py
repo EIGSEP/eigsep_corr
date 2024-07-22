d982b0f9a514cfeb476dd47dbfdf70b0185f19fd
EIGSEP Pi <chb@berkeley.edu>
2024-07-21 17:08:24 -0700

    plot all correlations

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -4,7 +4,8 @@ from eigsep_corr.plot import plot_live
 REDIS_HOST = "10.10.10.10"
 REDIS_PORT = 6379
 AUTOS = ["0", "1", "2", "3", "4", "5"]
-pairs = ["2", "3"]
+#pairs = ["2", "3"]
 
 r = redis.Redis(REDIS_HOST, port=REDIS_PORT)
-plot_live(r, pairs=AUTOS, plot_delay=False, log_scale=True)
+#plot_live(r, pairs=AUTOS, plot_delay=False, log_scale=True)
+plot_live(r, plot_delay=False, log_scale=True)
