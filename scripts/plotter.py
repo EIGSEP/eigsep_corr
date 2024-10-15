4eadf7c3da0082cf65a83361654989d217bbb3ff
EIGSEP Pi <chb@berkeley.edu>
2024-10-15 14:37:42 -0700

    plot all autos

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -7,5 +7,5 @@ AUTOS = ["0", "1", "2", "3", "4", "5"]
 #pairs = ["2", "3"]
 
 r = redis.Redis(REDIS_HOST, port=REDIS_PORT)
-#plot_live(r, pairs=AUTOS, plot_delay=False, log_scale=True)
-plot_live(r, pairs=["3"], plot_delay=False, log_scale=True)
+plot_live(r, pairs=AUTOS, plot_delay=False, log_scale=True)
+#plot_live(r, pairs=["3"], plot_delay=False, log_scale=True)
