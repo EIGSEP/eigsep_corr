0dda9a002748aea4de94513bab08b6169beae5c0
Christian Hellum Bye <chbye@berkeley.edu>
2025-06-27 19:34:28 -0700

    add flag for computing inttime

--- eigsep_corr/config.py
+++ eigsep_corr/config.py
@@ -4,7 +4,7 @@ import yaml
 from .utils import calc_inttime
 
 
-def load_config(name):
+def load_config(name, compute_inttime=True):
     """
     Load the configuration file.
 
@@ -12,26 +12,29 @@ def load_config(name):
     ----------
     name : str or Path
         Path to the configuration file.
+    compute_inttime : bool
+        Compute integration time and file time if True.
 
     Returns
     -------
     dict
         Configuration parameters.
+
     """
     config_path = Path(name)
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
-    # useful computed quantities
-    sample_rate = config["sample_rate"]
-    corr_acc_len = config["corr_acc_len"]
-    acc_bins = config["acc_bins"]
-    t_int = calc_inttime(
-        sample_rate * 1e6,  # in Hz
-        corr_acc_len,
-        acc_bins=acc_bins,
-    )
-    ntimes = config["ntimes"]
-    file_time = t_int * ntimes
-    config["integration_time"] = t_int
-    config["file_time"] = file_time
+    if compute_inttime:
+        sample_rate = config["sample_rate"]
+        corr_acc_len = config["corr_acc_len"]
+        acc_bins = config["acc_bins"]
+        t_int = calc_inttime(
+            sample_rate * 1e6,  # in Hz
+            corr_acc_len,
+            acc_bins=acc_bins,
+        )
+        ntimes = config["ntimes"]
+        file_time = t_int * ntimes
+        config["integration_time"] = t_int
+        config["file_time"] = file_time
     return config
